faeebdf7292ee9ed7e99c30c22d6c9055f70ead5
Theodore Fong <theodore92fong@gmail.com>
2020-03-10 19:17:31 -0500

    Updating V4Q Folder

---
+++ RubyTutorial/spec/make_an_appointment_spec.rb
@@ -0,0 +1,16 @@
+describe "A user can login and make an appointment" do
+
+	    it 'Given I want to make an appointment'
+
+	    it 'and I enter a valid username'
+
+	    it 'and I enter a valid password'
+
+			it 'When I click the Login button'
+
+			it 'and I enter my appointment information'
+
+			it 'and I click the Book Appointment button'
+
+	    it 'Then I get a confirmation page'
+end
---
+++ RubyTutorial/spec/must_have_password_spec.rb
@@ -0,0 +1,14 @@
+
+describe 'A user must have a password to make an appointment' do
+
+    it 'Given I want to make an appointment'
+
+    it 'and I enter a valid username'
+
+    it 'When I enter a bad password'
+
+    it 'and I click the Login button'
+
+    it 'Then I get an error message'
+
+end
